f46bc027a56cd316a7c11bf4da05bf5e97f8326a
Christopher Bruton <chris@bruton.tech>
2021-01-27 20:47:29 -0800

    add passthrough directories

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
  
-  eleventyConfig.setTemplateFormats(["css", "html", "njk", "md"]);
 };
 
