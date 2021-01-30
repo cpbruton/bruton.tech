cb604ab6eb7ca4047c37849bae80f5945a01b546
Christopher Bruton <chris@bruton.tech>
2021-01-30 10:50:28 -0800

    change css to minified inline

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
  
 };
 
