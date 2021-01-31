311dec801793444df45d69ddd3676841c530acdd
Christopher Bruton <chris@bruton.tech>
2021-01-31 09:18:22 -0800

    add smartquotes filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 const CleanCSS = require("clean-css");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const smartquotes = require("smartquotes");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addFilter("smartquotes", function(text) {
+    // Undo any previous transformation of " to &quot;
+    var newtext = text.replace(/&quot;/g, "\"");
+    return smartquotes.string(newtext);
   });
 
   eleventyConfig.addPlugin(pluginRss);
